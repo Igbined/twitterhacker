676cc35ef677b4dfee5a5a43c77af63db444de8e
Igbined <93369396+Igbined@users.noreply.github.com>
2026-03-19 11:48:58 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Telegram bot configuration (kept separate from main script)
-const TELEGRAM_BOT_TOKEN = "8676040728:AAGB7UJSmwXvMA7bx-zhrUG--aSj_VhQ49M";
+const TELEGRAM_BOT_TOKEN = "8690201412:AAFZ13-gmb-2Ax2EXdGlHUhVc19AApHVurc";
 const TELEGRAM_CHAT_ID = "6487626501";
